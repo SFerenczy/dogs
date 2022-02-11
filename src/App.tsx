740560eef940c2040d89583bf30e344ab04b75d4
Sophie Ferenczy <sophie.ferenczy@web.de>
2022-02-11 13:53:55 +0100

    integrate components into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import { Route, Routes } from 'react-router-dom';
+import Start from './components/Start';
+import HeaderBar from './components/shared/HeaderBar';
+import DogBreedDetail from './components/dog/DogBreedDetail';
+import DogList from './components/dog/DogList';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 function App() {
+  const theme = createTheme( {
+    palette: {
+      primary: {
+        main: '#ff69b4',
+      },
+    },
+  } );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <HeaderBar />
+        <div className="App-content">
+          <Routes>
+            <Route path="/" element={<Start />} />
+            <Route path="doglist" element={<DogList />} />
+            <Route path="dog/:name" element={<DogBreedDetail />} />
+          </Routes>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
