5c783de50732e98283c2ce7b8f44d8474d72026e
Sophie Ferenczy <sophie.ferenczy@web.de>
2022-02-14 17:02:41 +0100

    add comment

--- src/components/shared/HeaderBar.tsx
+++ src/components/shared/HeaderBar.tsx
@@ -2,6 +2,10 @@ import { ChevronLeft } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
 import { useMatch, useNavigate } from 'react-router-dom';
 
+/**
+ * The Header of the page. Contains the title of the app and a back-icon if on the breed-page.
+ * @returns A JSX.Element
+ */
 function HeaderBar(): JSX.Element {
   const match = useMatch( '/dog/:name' );
   const navigate = useNavigate();
